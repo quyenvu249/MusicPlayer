f21d8fc165ec8f454622fbad159d8c376b0770e0
quyenvu249 <quyenvu249@users.noreply.github.com>
2020-10-10 19:55:41 +0700

    source code

---
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/PlaylistFragment.java
@@ -0,0 +1,34 @@
+package com.example.musicplayer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlaylistFragment extends Fragment {
+
+    public PlaylistFragment() {
+        // Required empty public constructor
+    }
+
+    public static PlaylistFragment newInstance() {
+        PlaylistFragment fragment = new PlaylistFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_playlist, container, false);
+    }
+}
